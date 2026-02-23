1e934729f9001bc7b28345b893fbd8ffce337791
vinicius <viniven7@gmail.com>
2026-02-23 15:54:50 -0300

    feat: Created the Enum CategoryEnum.

---
+++ src/main/java/com/fridgeAI/MagicFridge/AI/model/CategoryEnum.java
@@ -0,0 +1,13 @@
+package com.fridgeAI.MagicFridge.AI.model;
+
+public enum CategoryEnum {
+
+    CARNES_E_OVOS,
+    LATICINIOS,
+    HORTALICAS,
+    OLEAGINOSAS_E_SEMENTES,
+    FRUTAS,
+    CEREAIS_E_GRAOS,
+    OLEOS_E_GORDURAS
+
+}
