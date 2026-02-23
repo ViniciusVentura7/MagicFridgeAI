3054f4e3ce222c85f310d2de221e7df3b9b3bc4e
vinicius <viniven7@gmail.com>
2026-02-23 16:03:23 -0300

    feat: Service and Controller classes implemented with Post, Get, Put and Delete annotations.

--- src/main/java/com/fridgeAI/MagicFridge/AI/controller/FoodItemController.java
+++ src/main/java/com/fridgeAI/MagicFridge/AI/controller/FoodItemController.java
@@ -2,6 +2,8 @@ package com.fridgeAI.MagicFridge.AI.controller;
 
 import com.fridgeAI.MagicFridge.AI.model.FoodItem;
 import com.fridgeAI.MagicFridge.AI.service.FoodItemService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -17,13 +19,26 @@ public class FoodItemController {
         this.foodItemService = foodItemService;
     }
 
-    @GetMapping
-    public List<FoodItem> getAll(){return foodItemService.getAll();}
+    @GetMapping("/list")
+    public ResponseEntity<List<FoodItem>> getAll() {
+        return ResponseEntity.ok(foodItemService.getAll());
+    }
 
-    @PostMapping
-    public FoodItem create(@RequestBody FoodItem food) { return foodItemService.save(food); }
+    @PostMapping("/add")
+    public ResponseEntity<FoodItem> addFood(@RequestBody FoodItem food) {
+        FoodItem savedFood = foodItemService.save(food);
+        return ResponseEntity.ok(savedFood);
+    }
 
-    @DeleteMapping
-    public void delete(@PathVariable Long id) {foodItemService.delete(id);}
+    @PutMapping("/change")
+    public ResponseEntity<FoodItem> changeFood(@PathVariable Long id, @RequestBody FoodItem food) {
+        FoodItem updatedFood = foodItemService.change(id, food);
+        return ResponseEntity.ok(updatedFood);
+    }
 
+    @DeleteMapping("/delete")
+    public ResponseEntity<Void> delete (@PathVariable Long id){
+        foodItemService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/fridgeAI/MagicFridge/AI/service/FoodItemService.java
+++ src/main/java/com/fridgeAI/MagicFridge/AI/service/FoodItemService.java
@@ -6,6 +6,7 @@ import com.fridgeAI.MagicFridge.AI.repository.FoodItemRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class FoodItemService {
@@ -17,13 +18,36 @@ public class FoodItemService {
     }
 
     //Listar
-    public List<FoodItem> getAll(){return foodItemRepository.findAll();}
+    public List<FoodItem> getAll() {
+        return foodItemRepository.findAll();
+    }
+
+    //Listar por id
+    public FoodItem listFoodById(Long id) {
+        Optional<FoodItem> foodById = foodItemRepository.findById(id);
+        return foodById.orElse(null);
+    }
 
     //Criar
-    public FoodItem save(FoodItem food) { return  foodItemRepository.save(food);}
+    public FoodItem save(FoodItem food) {
+        return foodItemRepository.save(food);
+    }
 
     //Deletar
-    public void delete(Long id) { foodItemRepository.deleteById(id);}
+    public void delete(Long id) {
+        foodItemRepository.deleteById(id);
+    }
 
+    //Alterar
+    public FoodItem change(Long id, FoodItem food) {
+        FoodItem updatedFood = foodItemRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Food not found."));
+        updatedFood.setId(food.getId());
+        updatedFood.setName(food.getName());
+        updatedFood.setCategory(food.getCategory());
+        updatedFood.setExpirationDate(food.getExpirationDate());
+
+        return foodItemRepository.save(updatedFood);
+    }
 
 }
