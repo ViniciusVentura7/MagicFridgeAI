32c570935d2360acc404c4192e2d9b66e7d68325
vinicius <viniven7@gmail.com>
2026-02-23 16:00:01 -0300

    refactor: Getter and Setters annotations implemented in FoodItem.

--- src/main/java/com/fridgeAI/MagicFridge/AI/model/FoodItem.java
+++ src/main/java/com/fridgeAI/MagicFridge/AI/model/FoodItem.java
@@ -3,53 +3,27 @@ package com.fridgeAI.MagicFridge.AI.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.lang.model.element.Name;
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "food_item")
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 public class FoodItem {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @Enumerated(EnumType.STRING)
+    private CategoryEnum category;
     private LocalDate expirationDate;
     private Integer quantity;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public LocalDate getExpirationDate() {
-        return expirationDate;
-    }
-
-    public void setExpirationDate(LocalDate expirationDate) {
-        this.expirationDate = expirationDate;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
 }
